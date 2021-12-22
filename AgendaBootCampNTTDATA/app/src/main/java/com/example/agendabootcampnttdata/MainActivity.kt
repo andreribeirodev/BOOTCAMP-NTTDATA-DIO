e92ee4b1541e6a9340490afb66fcda20d438ecab
andreribeirodev <andreeleeandro@gmail.com>
2021-12-22 00:43:37 -0300

    Faz alterações no Layout

--- AgendaBootCampNTTDATA/app/src/main/java/com/example/agendabootcampnttdata/MainActivity.kt
+++ AgendaBootCampNTTDATA/app/src/main/java/com/example/agendabootcampnttdata/MainActivity.kt
@@ -5,20 +5,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CalendarContract
-import android.widget.Button
 import android.provider.CalendarContract.Events.*
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    var btnSetEvent = findViewById<MaterialButton>(R.id.btnSetEvent)
+    var edtName = findViewById<MaterialTextView>(R.id.edtName)
+    var edtLocation = findViewById<MaterialTextView>(R.id.edtLocation)
+    var edtDescription = findViewById<MaterialTextView>(R.id.edtDescription)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSetEvent = findViewById<Button>(R.id.btnSetEvent)
+        addEvent()
+
+    }
+
+    private fun addEvent() {
         btnSetEvent.setOnClickListener {
 
             val intent = Intent(Intent.ACTION_INSERT)
                 .setData(CONTENT_URI)
-                .putExtra(TITLE, "Bootcamp everis")
+                .putExtra(TITLE,"Luiz Gonzaga")
                 .putExtra(EVENT_LOCATION, "ONLINE")
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
                 .putExtra(
@@ -28,5 +40,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
+
 
